2d8d20ad256bb7d1ecca2157bc48dacd35723e36
Pipin10 <61896418+Pipin10@users.noreply.github.com>
2020-06-11 20:44:11 +0700

    Tugas Lab 7 - OOP Bagian Akhir (Abstraction)

---
+++ Program.cs
@@ -0,0 +1,57 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+//menggunakan using Abstraction.AbstractClass;    // dengan untuk Abstract Class
+using Abstraction.Interface;      // dengan untuk Interface
+
+namespace Abstraction
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            /*AlasKaki;          // Menggunakan Abstract Class
+
+            Console.WriteLine();
+            AlasKaki = new Wedges();
+            AlasKaki.Sepatu();
+            Console.WriteLine();
+
+            AlasKaki = new Flatshoes();
+            AlasKaki.Sepatu();
+            Console.WriteLine();
+
+            AlasKaki = new Sneakers();
+            AlasKaki.Sepatu();
+            Console.WriteLine();
+
+            AlasKaki = new Highheels();
+            AlasKaki.Sepatu();
+            Console.WriteLine();*/
+
+            IAlasKaki AlasKaki;       // Menggunakan Interface
+
+            Console.WriteLine();
+            AlasKaki = new Wedges();
+            AlasKaki.Sepatu();
+            Console.WriteLine();
+
+            AlasKaki = new Flatshoes();
+            AlasKaki.Sepatu();
+            Console.WriteLine();
+
+            AlasKaki = new Sneakers();
+            AlasKaki.Sepatu();
+            Console.WriteLine();
+
+            AlasKaki = new Highheels();
+            AlasKaki.Sepatu();
+            Console.WriteLine();
+
+            Console.ReadKey();
+        }
+    }
+}
